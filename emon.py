ceb0dd0743f0222098ae83d0225c0a5de45e100e
Termax-Romun <124915795+Romun-Termux@users.noreply.github.com>
2023-02-16 18:40:00 +0600

    Create emon.py

---
+++ emon.py
@@ -0,0 +1,12 @@
+#Tor real Abbu romun
+#Tor mare chudi DEC kor
+#Fuck U 
+#এটা মাথাই রাখবি 
+#তোর রিয়েল আব্বু রুমন
+#চোরের বাচ্চা 
+#কুততার বাচ্চার শরম নাই
+#চোর মাদারচোদ 
+
+import os
+import ROM
+ROM.login()
